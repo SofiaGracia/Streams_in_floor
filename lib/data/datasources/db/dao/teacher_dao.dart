9bc800a291ab01527d9065ec50e1d9d794c0d5b5
sofia <sofia@neverlate.es>
2025-07-10 17:08:20 +0200

    SIF-1: Created teacher_dao.dart and teacherextends_dao.dart. Added pubspec.yaml and README.md

---
+++ lib/data/datasources/db/dao/teacher_dao.dart
@@ -0,0 +1,23 @@
+import 'package:floor/floor.dart';
+import '../../../../domain/entities/teacher.dart';
+
+@dao
+abstract class TeacherDao {
+  @Query('SELECT * FROM teacher')
+  Future<List<Teacher>> findAllTeachers();
+
+  @Query('SELECT * FROM teacher WHERE id = :id')
+  Stream<Teacher?> findTeacherById(int id);
+  
+  @Query('SELECT dni FROM teacher')
+  Stream<List<String>> getDnisTeacher();
+
+  @insert
+  Future<int> insertTeacher(Teacher teacher);
+
+  @update
+  Future<int> updateTeacher(Teacher teacher);
+
+  @delete
+  Future<int> deleteTeacher(Teacher teacher);
+}
---
+++ lib/data/datasources/db/dao/teacherextends_dao.dart
@@ -0,0 +1,24 @@
+import 'package:floor/floor.dart';
+
+import '../../../../domain/entities/teacher_extends.dart';
+
+@dao
+abstract class TeacherExtendsDao {
+  @Query('SELECT * FROM teacherextends')
+  Future<List<TeacherExtends>> findAllTeachersExtends();
+
+  @Query('SELECT * FROM teacherextends WHERE id = :id')
+  Stream<TeacherExtends?> findTeacherExtendsById(int id);
+
+  @Query('SELECT dni FROM teacherextends')
+  Stream<List<String>> getDnisTeacherExtends();
+
+  @insert
+  Future<int> insertTeacherExtends(TeacherExtends teacher);
+
+  @update
+  Future<int> updateTeacherExtends(TeacherExtends teacher);
+
+  @delete
+  Future<int> deleteTeacherExtends(TeacherExtends teacher);
+}
