698908332f8c971aa3e740c5f6e722947f3db5e6
sofia <sofia@neverlate.es>
2025-07-10 18:39:56 +0200

    SIF-7: Created TeacherListScreen

---
+++ lib/presentation/screens/teacher_list.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../providers/teacher_repository.dart';
+
+class TeacherListScreen extends ConsumerWidget {
+  const TeacherListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final teachersAsync = ref.watch(teacherListDnisProvider);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Teachers Dnis')),
+      body: teachersAsync.when(
+        data: (teachersDnis) => ListView.builder(
+          itemCount: teachersDnis.length,
+          itemBuilder: (_, i) {
+            final t = teachersDnis[i];
+            return ListTile(
+              title: Text(t),
+              subtitle: Text('DNI: ${t}'),
+            );
+          },
+        ),
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (e, _) => Center(child: Text('Error: $e')),
+      ),
+    );
+  }
+}
