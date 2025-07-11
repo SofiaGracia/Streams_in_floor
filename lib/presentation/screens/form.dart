adddb138d4d2f56544c28be3f705f77c1823791d
sofia <sofia@neverlate.es>
2025-07-11 13:58:51 +0200

    SIF-10: Updated FormScreen

--- lib/presentation/screens/form.dart
+++ lib/presentation/screens/form.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:streams_in_floor/domain/entities/teacher.dart';
+import 'package:streams_in_floor/presentation/providers/teacher_repository.dart';
 
 import '../widgets/save_form_button.dart';
 
@@ -27,18 +29,34 @@ class _FormScreenState extends ConsumerState<FormScreen> {
     nameController = TextEditingController();
   }
 
-  //TODO: Write a method for saving a new teacher
+  Future<void> _saveNewTeacher() async {
+    final newTeacher = Teacher(
+      name: nameController.text.trim(),
+      dni: dniController.text.trim(),
+    );
+
+    final repo = ref.read(teacherRepositoryProvider);
+    await repo.addTeacher(newTeacher);
+
+    if (mounted) Navigator.pop(context);
+  }
+
+  String? _notEmptyValidator(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'This field is required';
+    }
+    return null;
+  }
 
   Widget _buildTextField({
     required TextEditingController controller,
     required String label,
-    String? Function(String?)? validator,
     required IconData icon,
     TextCapitalization textCapitalization = TextCapitalization.none,
   }) {
     return TextFormField(
       controller: controller,
-      validator: validator,
+      validator: _notEmptyValidator,
       textCapitalization: textCapitalization,
       decoration: InputDecoration(
         labelText: label,
@@ -63,16 +81,20 @@ class _FormScreenState extends ConsumerState<FormScreen> {
                 _buildTextField(
                     controller: dniController,
                     label: 'ID or DNI',
-                    icon: Icons.account_circle_outlined),
+                    icon: Icons.perm_identity_outlined),
                 const SizedBox(height: 12),
                 _buildTextField(
-                    controller: dniController,
-                    label: 'ID or DNI',
-                    icon: Icons.account_circle_outlined),
+                    controller: nameController,
+                    label: 'Name',
+                    icon: Icons.person_outline),
                 const SizedBox(height: 12),
                 //Save teacher button
                 SaveTeacherButton(
-                  onPressed: ,
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      _saveNewTeacher();
+                    }
+                  },
                   text: 'Save',
                 )
               ],
