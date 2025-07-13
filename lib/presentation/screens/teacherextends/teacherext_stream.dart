c3a525838179cc7c51e3974f892fbc79f45921b6
sofia <sofia@neverlate.es>
2025-07-13 16:28:21 +0200

    SIF-26: Created StreamTeacherExtListScreen

--- lib/presentation/screens/teacherextends/teacherext_stream.dart
+++ lib/presentation/screens/teacherextends/teacherext_stream.dart
@@ -1,14 +1,13 @@
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:streams_in_floor/presentation/providers/teacher/teacher_stream.dart';
+import 'package:streams_in_floor/presentation/providers/teacherextends/teacherext_stream.dart';
 import 'package:streams_in_floor/presentation/screens/teacherextends/teacherext_list.dart';
 
 import '../../../shared/utils/constants.dart';
-import '../../providers/teacherextends/teacherext_async_notifier.dart';
 
-class AsyncTeacherExtListScreen extends ConsumerWidget {
-  const AsyncTeacherExtListScreen({super.key});
+class StreamTeacherExtListScreen extends ConsumerWidget {
+  const StreamTeacherExtListScreen({super.key});
 
   Widget _buildScreen(Widget passedChild){
     return Scaffold(
@@ -20,11 +19,11 @@ class AsyncTeacherExtListScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final teachersExtAsync = ref.watch(teacherExtNotifierProvider);
+    final teacherExtendsAsync = ref.watch(teacherExtendsStreamProvider);
 
-    return teachersExtAsync.when(
+    return teacherExtendsAsync.when(
         data: (teachers) {
-          return TeacherExtListScreen(teachers: teachers, titleScreen: asyncNotifierTitleScreen);
+          return TeacherExtListScreen(teachers: teachers, titleScreen: streamNotifierTitleScreen);
         },
         error: (e, stack) {
           return _buildScreen(Text('Error: $e'));
